0dd25d2ce703b6af877b70a5c961290415560ddd
Alfara15 <90241420+Alfara15@users.noreply.github.com>
2021-10-11 17:13:20 +0700

    tambah email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .card-header{
+    background-color: black;
+    font-size: 22px;
+    text-align: center;
+    color: white;
+    }
+    .form-group{
+    margin-left: -150px;
+    text-align: center;
+    }
+    .btn{
+        font-size: 17px;
+        text-align: center;
+        background: #333333;
+        border-radius: 37px;
+        border: none;
+        box-shadow: 0px 1px 8px black;
+        cursor: pointer;
+        color: white;
+        font-family: "Raleway SemiBold", sans-serif;
+        height: 42.3px;
+        margin: 0 auto;
+        margin-top: 50px;
+        transition: 0.25s;
+        width: 153px;
+    }
+   .btn:hover{
+       background-color: black;
+       color: white;
+   }
+    .form-control{
+        border-color:  black;
+    }
+    .card{
+       background: #fbfbfb;
+       border-radius: 10px;
+       box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.65);
+    }
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,7 +56,7 @@
 
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
-
+    
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -34,7 +74,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Reset Password') }}
                                 </button>
                             </div>
                         </div>
